89ed3ce4ca146b1cb98bd71da3129e81b4669f2c
John Cooper <johnxcooper87@gmail.com>
2019-04-05 19:04:37 +0700

    update

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,12 +3,12 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { DuplicatesPlugin } from 'inspectpack/plugin';
+//import TerserPlugin  from 'terser-webpack-plugin';
 
 export default {
   devtool: 'inline-source-map',
   mode: 'development',
   cache: true,
-
   target: 'web',
 
   entry: './test/app.js',
@@ -31,8 +31,11 @@ export default {
         options: fs.readFileSync('.babelrc') |> JSON.parse
       },
       {
-        test: /\.flow$/,
-        loader: 'ignore-loader'
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
       }
     ]
   },
@@ -43,12 +46,17 @@ export default {
       title: 'graphql-server',
       template: './test/index.html'
     }),
-    new DuplicatesPlugin({
-      verbose: true
-    }),
+    new DuplicatesPlugin({})
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     'NODE_ENV': JSON.stringify('"production"')
+    //   }
+    // }),
   ],
 
   optimization: {
+    //minimize: true,
+    //minimizer: [new TerserPlugin()],
     namedChunks: true,
     namedModules: false,
     splitChunks: {
@@ -62,16 +70,22 @@ export default {
     }
   },
 
+  // resolve: {
+  //   alias: {
+  //     'graphql': path.resolve('./node_modules/graphql')
+  //   }
+  // }
+
   devServer: {
-    before: function(app) {
-      app.get('/graphiql.css', function(req, res) { 
-        res.sendFile( path.resolve(__dirname, 'node_modules/graphiql/graphiql.css') );
-      });
-    },
+    // before: function(app) {
+    //   app.get('/graphiql.css', function(req, res) { 
+    //     res.sendFile( path.resolve(__dirname, 'node_modules/graphiql/graphiql.css') );
+    //   });
+    // },
     proxy: {
       '/api': 'http://localhost:8080/graphql'
     },
-    contentBase: './test',
+    contentBase: './test/server',
     historyApiFallback: true,
     watchContentBase: true,
     port: 3000
