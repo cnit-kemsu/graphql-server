35dad5d768af25554efae038810bb2988b98eb60
John Cooper <johnxcooper87@gmail.com>
2019-04-16 10:12:51 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 export * as types from './src/graphql/types';
-export { Mapping } from './src/graphql/Mapping';
+export { Mapping, jsonArray } from './src/graphql/Mapping';
 export { Loader } from './src/graphql/Loader';
 export { compose } from './src/graphql/compose';
+export { authorize } from './src/graphql/authorize';
+export { graphqlResolver } from './src/graphql/resolver';
+export { verifyPassword, hashPassword } from './src/auth/pwdhash';
+export { userInfo } from './src/auth/userInfo';
+export { errorLogger } from './src/error/errorLogger';
+export { handleUncaughtErrors } from './src/error/handleUncaughtErrors';
+export { PublicError } from './src/error/PublicError';
+export { Pool } from './src/database/Pool';
