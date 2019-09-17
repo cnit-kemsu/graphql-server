34cbfd1c1612178dba4c5f8425e3cebc9d639188
John Cooper <johnxcooper87@gmail.com>
2019-09-17 15:41:31 +0700

    update

--- src/graphql/compose.js
+++ src/graphql/compose.js
@@ -1,35 +1,22 @@
 import { GraphQLSchema, GraphQLObjectType } from 'graphql';
 
-function groupByIndex([queryFields, mutationFields, loaders], element) {
-  return [{
-    ...queryFields,
-    ...element[0]
-  }, {
-    ...mutationFields,
-    ...element[1]
-  }, {
-    ...loaders,
-    ...element[2]
-  }];
-}
+export function compose(schemaComponents) {
+  const queryFields = {};
+  const mutationFields = {};
+  const loaders = {};
 
-export function compose(elements) {
+  for (const component of schemaComponents) {
+    Object.assign(queryFields, component.query);
+    Object.assign(mutationFields, component.mutation);
+    Object.assign(loaders, component.loaders);
+  }
 
-  return elements.reduce(groupByIndex, [ {}, {}, {} ])
-  |> {
+  let query, mutation;
+  if (Object.keys(queryFields).length > 0) mutation = new GraphQLObjectType({ name: 'Query', fields: queryFields });
+  if (Object.keys(mutationFields).length > 0) mutation = new GraphQLObjectType({ name: 'Mutation', fields: mutationFields });
 
-    schema: new GraphQLSchema({
-      query: new GraphQLObjectType({
-        name: 'Query',
-        fields: #[0]
-      }),
-      mutation: new GraphQLObjectType({
-        name: 'Mutation',
-        fields: #[1]
-      })
-    }),
-
-    loaders: #[2]
-
+  return {
+    schema: new GraphQLSchema({ query, mutation }),
+    loaders
   };
 }
--- src/graphql/upgradeResolveFn.js
+++ src/graphql/upgradeResolveFn.js
@@ -4,6 +4,7 @@ const AsyncFunction = Object.getPrototypeOf(async function() {}).constructor;
 
 export function upgradeResolveFn(target) {
   const resolveFn = target.resolve;
+  
   if (resolveFn.constructor === AsyncFunction) {
     target.resolve = async function (obj, args, context, info) {
       return await resolveFn(obj, args, context, graphqlFields(info));
