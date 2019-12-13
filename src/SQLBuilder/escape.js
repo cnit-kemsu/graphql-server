8f434e3846d6c6ffd1da91e96f86aa4d96d62d47
unknown <johnxcooper87@gmail.com>
2019-12-13 16:51:00 +0700

    update

---
+++ src/SQLBuilder/escape.js
@@ -0,0 +1,29 @@
+//.replace(/(?<!\\)'/g, '\\\''); // finds all `'` (but not `\'`) and replaces with `\'`
+//.replace(/(\\|'|")/g, '\\$1'); // finds all `\`, `'` or `"` and adds `\` to it
+
+//.replace('\'', '\\\''); // finds all `'` and replaces with `\'`
+//.slice(1, str1.length - 1); // deletes first and last characters
+
+//https://mariadb.com/kb/en/library/string-literals/
+
+/**
+ * \0
+ * \'
+ * \"
+ * \b
+ * \n
+ * \r
+ * \t
+ * \Z
+ * \\
+ * \%
+ * \_
+ */
+
+export function escape(value) {
+  return JSON.stringify(value);
+}
+
+export function escapePattern(value) {
+  return value.replace(/(%|_)/g, '\\$1');
+}
--- src/SQLBuilder/test.js
+++ src/SQLBuilder/test.js
@@ -1,135 +1,77 @@
-// import { SQLBuilder } from './';
+import { SQLBuilder } from './';
+import { escape, escapePattern } from './escape';
 
 
+const sum = (a, b) => a + b;
+test('lalatest', () => {
+  //expect(sum(1, 1)).toBe(3);
 
-// const sum = (a, b) => a + b;
-// test('lalatest', () => {
-//   //expect(sum(1, 1)).toBe(3);
-
-//   const selectExprListBuilder = {
-//     name: '_name',
-//     surname: null,
-//     email: 'email',
-//     friendsKeys() { return '(SELECT id FROM friends WHERE person_1 = id)'; },
-//     school: { schoolId: 'school_id', city: null },
-//     city({ cityId = 1 }) { return [`(SELECT name FROM cities WHERE id = ?)`, cityId]; },
-//   };
+  const selectExprListBuilder = {
+    name: '_name',
+    surname: null,
+    email: 'email',
+    friendsKeys() { return '(SELECT id FROM friends WHERE person_1 = id)'; },
+    school: { schoolId: 'school_id', city: null },
+    city({ cityId = 1 }) { return [`(SELECT name FROM cities WHERE id = ?)`, cityId]; },
+  };
   
-//   const whereConditionBuilder = {
-//     name: '_name',
-//     middlename: 'middlename1',
-//     //surname: null,
-//     cityId(cityId) { return [`city_id != ?`, cityId]; },
-//     schoolId(schoolId) { return [`schoolId != ?`, schoolId]; },
-//   };
+  const whereConditionBuilder = {
+    name: '_name',
+    middlename: 'middlename1',
+    //surname: null,
+    cityId(cityId) { return [`city_id != ?`, cityId]; },
+    schoolId(schoolId) { return [`schoolId != ?`, schoolId]; },
+  };
   
-//   const assignmentListBuilder = {
-//     name: '_name',
-//     //surname: null,
-//     async schoolId({ schoolId }) { return ['school_id = ?, city_id = (SELECT city_id FROM schools WHERE school_id = ?)', schoolId, schoolId]; }
-//   };
+  const assignmentListBuilder = {
+    name: '_name',
+    //surname: null,
+    async schoolId({ schoolId }) { return ['school_id = ?, city_id = (SELECT city_id FROM schools WHERE school_id = ?)', schoolId, schoolId]; }
+  };
   
-//   const sqlBuilder = new SQLBuilder(selectExprListBuilder, whereConditionBuilder, assignmentListBuilder);
-//   const whereClause = sqlBuilder.buildWhereClause({ name: 'lalala', middlename: ['eee1', 'uuu1'], surname: ['eee', 'uuu'], email: 'qwerty', cityId: 2, schoolId: ['1', 'a'] });
-//   //console.log(whereClause);
+  const sqlBuilder = new SQLBuilder(selectExprListBuilder, whereConditionBuilder, assignmentListBuilder);
+  const whereClause = sqlBuilder.buildWhereClause({ name: 'lalala', middlename: ['eee1', 'uuu1'], surname: ['eee', 'uuu'], email: 'qwerty', cityId: 2, schoolId: ['1', 'a'] });
+  //console.log(whereClause);
 
-//   expect(whereClause).toEqual([
-//     'WHERE (name = ?) AND (middlename IN (?, ?)) AND (surname IN (?, ?)) AND (email = ?) AND (city_id != ?) AND (schoolId != ?)',
-//     ['lalala', 'eee1', 'uuu1', 'eee', 'uuu', 'qwerty', 2, ['1', 'a'] |> JSON.stringify]
-//   ]);
+  expect(whereClause).toEqual([
+    'WHERE (name = ?) AND (middlename IN (?, ?)) AND (surname IN (?, ?)) AND (email = ?) AND (city_id != ?) AND (schoolId != ?)',
+    ['lalala', 'eee1', 'uuu1', 'eee', 'uuu', 'qwerty', 2, ['1', 'a'] |> JSON.stringify]
+  ]);
 
-// });
+});
 
-// // const selectExprList = sqlBuilder.buildSelectExprList({ name: {}, surname: null, email: null, cityId: 'city_id', school: { name: null, pupleCount: {} }, city: null }, { cityId: 2 });
-// // const whereClause = sqlBuilder.buildWhereClause({ name: 'lalala', surname: ['eee', 'uuu'], email: 'qwerty', cityId: 2 });
-// // console.log(selectExprList);
-// // console.log(whereClause);
-// // (async function lalala () {
-// //   const assignmentList = await sqlBuilder.buildAssignmentList({ name: 'lalala', surname: 'tututu', email: 'asdfgh', schoolId: 3 });
-// //   console.log(assignmentList);
-// // })()
+// const selectExprList = sqlBuilder.buildSelectExprList({ name: {}, surname: null, email: null, cityId: 'city_id', school: { name: null, pupleCount: {} }, city: null }, { cityId: 2 });
+// const whereClause = sqlBuilder.buildWhereClause({ name: 'lalala', surname: ['eee', 'uuu'], email: 'qwerty', cityId: 2 });
+// console.log(selectExprList);
+// console.log(whereClause);
+// (async function lalala () {
+//   const assignmentList = await sqlBuilder.buildAssignmentList({ name: 'lalala', surname: 'tututu', email: 'asdfgh', schoolId: 3 });
+//   console.log(assignmentList);
+// })()
 
-// const whereBuilder = {
-//   prop1: '_prop1',
-//   prop2: name => ['name = ?', name],
-//   // prop3: (name1, name2) => [
-//   //   plh => [`name1 = ${plh}`, name1],
-//   //   plh => [`name2 = ${plh}`, name2]
-//   // ],
-//   prop4: (name1, name2) => [
-//     [fill => `name1 = ${fill('?', ', ')}`, name1],
-//     [ph => `name2 = ${ph}`, name2]
-//   ],
-//   email: value => `email = '${value}'`,
-//   keys: values => `id IN (${values.join(', ')})`,
-//   fullnameSearch: values => values.split(' ').map(value => `(firstname LIKE '%${value}%') OR lastname LIKE '%${value}%')`).join(' AND '),
-
-// };
-
-// const arr = [1, 2, 3];
-// console.log(arr.map(() => '?').join(', '));
-
-// import { Pool } from '../database/Pool';
-
-// const dbConfig = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'keepthefaith',
-//   database: 'openedu',
-//   port: 3306,
-//   multipleStatements: true   '       \'
-// };
-
-// const pool = new Pool(dbConfig);
-
-//let name = 'course';
-// let name = `course%'; \ SELECT'' \\' sd ' \ \\ * FROM users; % \n \\n SELECT * FROM course_design_templates WHERE _name LIKE \\'%course`;
-// console.log(name |> JSON.stringify);
-// console.log(name);
-// name = name.replace(/\\/g, "\\\\").replace(/'/g, "\\'");
-// //name = name.match(/[^\\]'/g);
-// const res = name.match(/^(?!\\)(')/g);
-
-// console.log(name);
-//console.log(res);
-
-// async function test() {
-  
-//   const db = await pool.getConnection();
-//   const courses = await db.query(`SELECT * FROM course_design_templates WHERE _name LIKE '%${name}%';`);
-//   db.end();
-//   console.log(courses);
-// }
-
-// test();
-
-const _str = '\\\'qwerty\\\' \'asdfgh\' \'\'123456';
-let str = '';
-for (let index = 0; index < 80; index++) {
-  str += ' ' + _str;
+function searchWord(word) {
+  return `%${escapePattern(word)}%`
+  |> escape
+  |> `(firstname LIKE ${#}) OR lastname LIKE ${#})`;
 }
 
-let start = process.hrtime();
+const whereBuilder = {
+  prop1: '_prop1',
+  prop2: name => ['name = ?', name],
+  // prop3: (name1, name2) => [
+  //   plh => [`name1 = ${plh}`, name1],
+  //   plh => [`name2 = ${plh}`, name2]
+  // ],
+  prop4: (name1, name2) => [
+    [fill => `name1 = ${fill('?', ', ')}`, name1],
+    [ph => `name2 = ${ph}`, name2]
+  ],
+  email: value => `email = '${escape(value)}'`,
+  keys: values => `id IN (${values.join(', ')})`,
+  searchText: text => text
+    .trim().replace(/\s{2,}/g, '')
+    .split(' ')
+    .map(searchWord)
+    .join(' AND ')
 
-let str2 = '';
-str2 = str.replace(/(?<!\\)'/g, '\\\'');
-//str2 = str.match(/'/g);
-
-let time = process.hrtime(start);
-console.log(time);
-
-start = process.hrtime();
-
-let str1 = '';
-
-for (let index = 1; index < str.length - 1; index++) {
-  if (str[index] == '\'' && str[index - 1] != '\\') {
-    str1 += '\\\'';
-  } else str1 += str[index];
-}
-
-time = process.hrtime(start);
-console.log(time);
-
-// console.log(str2);
-// console.log(str1);
+};
