0a776694e2621255d5fc02f989683501becb14b0
unknown <johnxcooper87@gmail.com>
2019-12-26 10:29:21 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export { types } from './src/graphql/types';
 export { resolveJSON } from './src/graphql/resolvers';
 export { SQLBuilder } from './src/SQLBuilder';
-export { escape, escapePattern } from './src/SQLBuilder/escape';
+export { escape, escapePattern, jsonToString, dateToString } from './src/SQLBuilder/escape';
 export { getJSON, setJSON } from './src/SQLBuilder/sql-json';
 export { upgradeResolveFn } from './src/graphql/upgradeResolveFn';
 export { Loader } from './src/Loader';
